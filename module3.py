c33a3efd5229eff32f3a32ac5e2a94eb1747f06c
siekmanna <drewsiekmann@gmail.com>
2024-02-27 14:04:00 -0600

    fixed some logic

--- module3.py
+++ module3.py
@@ -1,9 +1,11 @@
 sales_tax = .08
 tip = .18
 subtotal = float(input('Please enter the charge for the food: '))
-tax_amount = subtotal * sales_tax
-tip_amount = subtotal * tip
-total = subtotal + tax_amount + tip
+subtotal = round(subtotal, 2)
+tax_amount = round(subtotal * sales_tax, 2)
+total_minus_tip = round(subtotal + tax_amount, 2)
+tip_amount = round(total_minus_tip * tip, 2)
+total = round(total_minus_tip + tip_amount, 2)
 print('Subtotal: $' + str(subtotal))
 print('Tax: $' + str(tax_amount))
 print('Tip: $' + str(tip_amount))
